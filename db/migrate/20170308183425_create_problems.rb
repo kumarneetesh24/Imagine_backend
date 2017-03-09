0275acc9a81e6f6e4f9a0b08d4a4b1c32f5461c0
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-03-09 14:16:43 +0530

    [FEAT] added problem model

--- db/migrate/20170308183425_create_problems.rb
+++ db/migrate/20170308183425_create_problems.rb
@@ -1,7 +1,10 @@
 class CreateProblems < ActiveRecord::Migration[5.0]
   def change
     create_table :problems do |t|
-
+      t.string :pname
+      t.string :pcode
+      t.string :statement
+      t.boolean :state
       t.timestamps
     end
   end
