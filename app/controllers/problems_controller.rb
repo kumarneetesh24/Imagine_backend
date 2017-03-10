a57107d2bce896dcec157aba7de61efbea6c19f7
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-03-10 20:08:26 +0530

    [FEAT] added single problem response and response concern

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,8 +1,14 @@
 class ProblemsController < ApplicationController
+  include Response
 
   def index
-    @problems = Problem.all
+    @problems =  Problem.all
     json_response(@problems)
   end
 
+  def problem
+    pcode = params[:pcode]
+    @problem = Problem.where(pcode: pcode).first
+    json_response(@problem)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :problems
-
+  root to: 'home#index'
+  get '/problems' => 'problems#index', as: 'problems'
+  get '/problems/:pcode' => 'problems#problem', as: 'problem'
 end
