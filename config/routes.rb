1be992b0569b828a7d8458abbd39088b2c2134d4
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-03-11 21:34:58 +0530

    [FEAT] added routes for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  # root to: 'home#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount Sidekiq::Web => '/sidekiq'
   get '/problems' => 'problems#index', as: 'problems'
   get '/problems/:pcode' => 'problems#problem', as: 'problem'
+  post '/submit/' => 'submission#verify_submission', as: 'submit'
+  get 'get_submission/:submission_id' => 'submission#get_submission', as: 'get_submission'
 end
