3d10436e38fdca4086c9701f382e913ec2a18841
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-03-11 21:37:07 +0530

    [CHORE] making rails to render rails_admin not as api

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module Imagine
   class Application < Rails::Application
+    config.time_zone = 'Kolkata'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -25,6 +26,6 @@ module Imagine
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    # config.api_only = true
   end
 end
