2efb6257f5e639ec899adb03d6d5ba376cce54cc
Neetesh Kumar <kumarneetesh24@gmail.com>
2017-03-11 21:40:16 +0530

    [FEAT] added submission controller

---
+++ app/controllers/submission_controller.rb
@@ -0,0 +1,29 @@
+class SubmissionController < ApplicationController
+
+  include Response
+
+  def verify_submission
+    pcode = params[:pcode]
+    user_source_code = params[:user_source_code]
+    problem = Problem.by_pcode(pcode).first
+    json_response({error: 'wrong problem code'}, 500) && return if problem.nil?
+    submission = Submission.new(user_source_code: user_source_code)
+    problem.submissions << submission
+    submission.save!
+    ProcessSubmissionWorker.perform_async(submission_id: submission[:id].to_s)
+    redirect_to(get_submission_path(submission[:id])) && return
+  end
+
+  def get_submission
+    submission = Submission.by_id(params['submission_id']).first
+    msg = if submission.nil?
+            { error: 'bad submission' }
+          else
+            { id: submission[:id].to_s, status_code: submission[:status_code], error_desc: submission[:error_desc]  }
+          end
+    json_response(msg)
+    # respond_to do |format|
+    #   format.json { render json: msg }
+    # end
+  end
+end
